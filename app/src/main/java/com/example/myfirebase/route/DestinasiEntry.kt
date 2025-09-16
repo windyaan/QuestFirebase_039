3cad61e722aa747bafc89321e8074649642fde0a
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-16 16:27:47 +0700

    tambah DestinasiEntry.kt

---
+++ app/src/main/java/com/example/myfirebase/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.myfirebase.repositori.route
+
+import com.example.myfirebase.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
