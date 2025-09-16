b6ff6e44fe24bf8664ca82762ff6bf541f910f9b
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-16 16:27:27 +0700

    tambah DestinasiHome.kt

---
+++ app/src/main/java/com/example/myfirebase/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.myfirebase.repositori.route
+
+import com.example.myfirebase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
